575ce473273c7f49e232492469f363e8c165c821
Matik <mihon-epsilon@yandex.ru>
2024-09-04 16:54:23 +0300

    Update base.html and Add nav.html

--- todo_manager/todo_manager/urls.py
+++ todo_manager/todo_manager/urls.py
@@ -19,6 +19,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
 ]
